5e0a2654c6f29fd3cde36b31bb1bdf012cc76d04
opilane <opilane@HP-CZC2349HTN>
2024-03-12 11:41:52 +0200

    Add project files.

---
+++ MinuOmaModul.py
@@ -0,0 +1,25 @@
+﻿def loe_ankeet(fail:str)->any:
+    fail=open(fail,"r",encoding="utf-8")
+    kus=[]
+    vas=[]
+    #kus_vas={}
+    for line in fail:
+        n=line.find(":") #-разделитель,разграничитель
+        kus.append(line[0:n].strip())
+        vas.append(line[0+1:len(line)].strip())
+    
+        #k,v=line.strip().split(":") 
+        #kus_vas[k]=v #-работа со словорями
+
+    fail.close()
+    return kus,vas  #,kus_vas
+def loe_failist(fail:str)->list:
+    """
+    """
+    f=open(fail,"r",encoding="utf-8")
+    oiged=[]
+    for rida in f:
+        oiged.append(rida.strip())
+    f.close
+    return oiged
+
---
+++ Too.py
@@ -0,0 +1,14 @@
+﻿from MinuOmaModul import *
+
+loe_failist("oiged.txt")
+
+kus_vas={}
+
+küsimused, vastused=loe_ankeet("Ankeet.txt")
+print(küsimused)
+print(vastused)
+for i in range (len(küsimused)):
+    print(f"{i+1}. Küsimus on: "+küsimused[i]+", vastus on: "+vastused[i])
+
+
+
